fee6d0b5733e89c10a7a87c312a7a6feb2b61dd1
Valentin Poirot <valentin.poirot@outlook.com>
2023-10-06 09:51:41 +0200

    refacto + lru_cache

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # This is a sample Python script.
 import copy
+import functools
 import time
 
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 class Objet:
     def __init__(self, valeur, p1, p2, id):
@@ -13,15 +11,36 @@ class Objet:
         self.p2 = p2
         self.id = id
 
+    def __eq__(self, other):
+        return (self.valeur, self.p1, self.p2, self.id) == (other.valeur, other.p1, other.p2, other.id)
+
+    def __hash__(self):
+        return hash((self.valeur, self.p1, self.p2, self.id))
+
+
 class Sac:
     def __init__(self, p1Max, p2Max):
-
         self.p1Max = p1Max
         self.p2Max = p2Max
         self.p1 = 0
         self.p2 = 0
         self.objets = []  # This should be a list of Objet instances
         self.valeur = 0
+
+    def add_object(self, objet):
+        if (self.p1 + objet.p1 <= self.p1Max) and (self.p2 + objet.p2 <= self.p2Max):
+            self.p1 += objet.p1
+            self.p2 += objet.p2
+            self.objets.append(objet)
+            self.valeur += objet.valeur
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        self.objets.sort(key=lambda x: x.id)
+        return hash((self.p1Max, self.p2Max, self.p1, self.p2, self.valeur, tuple(self.objets)))
+
 
 def get_sac_from_file(path):
     try:
@@ -37,61 +56,41 @@ def get_sac_from_file(path):
         print(f"Failed to open file: {path}")
         exit(1)
 
+
 def print_sac(sac):
     print(f"P1: {sac.p1}\nP2: {sac.p2}\nValeur: {sac.valeur}")
     for i, objet in enumerate(sac.objets):
         print(f"Objet {i}: {objet.valeur} {objet.p1} {objet.p2}")
 
-def max_sac(a, b):
+
+def biggest_sac(a, b):
     return a if a.valeur >= b.valeur else b
 
-def is_feasible(sac, objet):
-    return sac.p1 + objet.p1 <= sac.p1Max and sac.p2 + objet.p2 <= sac.p2Max
 
-def put(sac, objet):
-    if is_feasible(sac, objet):
-        sac.p1 += objet.p1
-        sac.p2 += objet.p2
-        sac.objets.append(objet)  # Ajoute l'objet à la liste (agrandit dynamiquement la liste)
-        sac.valeur += objet.valeur
-        return sac
-    else:
-        return None
-
-
-def compute(sac, objets, memo=None):
-    if memo is None:
-        memo = {}
-
-    key = tuple(sorted(objet.id for objet in sac.objets))
-
-    if key in memo:
-        return memo[key]
-
+@functools.lru_cache(maxsize=None)
+def compute(sac, objets):
     if len(objets) == 0:
         return sac
 
-    cp_objets = copy.deepcopy(objets)
-    cp_objets.pop()
-    dont_take = compute(sac, cp_objets, memo)
+    dont_take = compute(copy.deepcopy(sac), objets[:-1])
 
-    takeSac = put(copy.deepcopy(sac), objets[-1])
-    if takeSac:
-        do_take = compute(takeSac, cp_objets, memo)
-        result = max_sac(do_take, dont_take)
+    take_sac = copy.deepcopy(sac)
+    res = take_sac.add_object(objets[-1])
+
+    if res:
+        do_take = compute(take_sac, objets[:-1])
+        best_sac = biggest_sac(do_take, dont_take)
     else:
-        result = max_sac(copy.deepcopy(sac), dont_take)
+        best_sac = dont_take
 
-    memo[key] = result
-    return result
-
+    return best_sac
 
 
 if __name__ == "__main__":
     # Enregistrement du temps de début
     start_time = time.time()
 
-    sac, objets = get_sac_from_file("objects/pb2.txt")
+    sac, objets = get_sac_from_file("objects/pb1.txt")
     sac_optimal = compute(sac, objets)
     print_sac(sac_optimal)
 
