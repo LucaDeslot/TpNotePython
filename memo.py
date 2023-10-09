1057c45f99d3ea1350d77ef3576240b60e86b2f0
Valentin Poirot <valentin.poirot@outlook.com>
2023-10-09 09:43:18 +0200

    create csv

--- memo.py
+++ memo.py
@@ -1,3 +1,5 @@
+import csv
+import os
 import time
 
 from bruteForce import get_sac_from_file
@@ -46,31 +48,57 @@ def sort_objects_by_potential(objets):
     return sorted(objets, key=lambda obj: obj.valeur / (obj.p1 + obj.p2), reverse=True)
 
 
+def save_results_to_csv(results, filename):
+    headers = ["Problem", "Max_Value", "Objects_Taken", "Avg_Execution_Time"]
+
+    # Check if file exists and is non-empty
+    file_exists = os.path.isfile(filename) and os.path.getsize(filename) > 0
+
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+
+        if not file_exists:
+            writer.writerow(headers)  # write header only if file does not exist
+
+        writer.writerows(results)
+
+
 if __name__ == "__main__":
-    sac, objets = get_sac_from_file("objects/pb8.txt")
-    # objets = sort_objects_by_potential(objets)
     timeouts = [10, 60, 120]  # your desired timeouts in seconds
 
-    for timeout in timeouts:
-        start_time = time.time()
+    for i in range(1, 9):  # loop from pb1 to pb8
+        results = []
 
-        # Clear the cache at the beginning of each run
-        memoization.cache.clear()
+        problem_name = f"pb{i}"
+        file_path = f"objects/{problem_name}.txt"
 
-        # Maintain the best solution found [value, items]
-        best_solution = [0, []]
+        total_time = 0
+        min_time = float("inf")
+        max_time = 0
 
-        try:
-            max_value, solution = compute_and_recover(len(objets) - 1, sac.p1Max, sac.p2Max, objets, start_time,
-                                                      timeout, best_solution)
-        except TimeoutError:
-            max_value, solution = best_solution  # Use the best found solution on timeout
+        # These variables might also need to be updated based on the variation in multiple runs
+        max_value = 0
+        objects_taken = []
 
-        end_time = time.time()
-        execution_time = end_time - start_time
+        sac, objets = get_sac_from_file(file_path)
+        objets = sort_objects_by_potential(objets)
+        for timeout in timeouts:
+            start_time = time.time()
+            # Clear the cache at the beginning of each run
+            memoization.cache.clear()
+            # Maintain the best solution found [value, items]
+            best_solution = [0, []]
+            try:
+                max_value, solution = compute_and_recover(len(objets) - 1, sac.p1Max, sac.p2Max, objets, start_time,
+                                                          timeout, best_solution)
+            except TimeoutError:
+                max_value, solution = best_solution  # Use the best found solution on timeout
+            end_time = time.time()
+            # Display results
+            print(f"\nResults for {problem_name}:")
+            print(f"\nResults with a timeout of {timeout} seconds:")
+            print(f"Max value: {max_value}")
+            print(f"Objects taken: {solution}")
+            results.append([problem_name, max_value, solution, timeout])
 
-        # Display results
-        print(f"\nResults with a timeout of {timeout} seconds:")
-        print(f"Max value: {max_value}")
-        print(f"Objects taken: {solution}")
-        print(f"Execution time: {execution_time} seconds")
+            save_results_to_csv(results, "results.csv")
